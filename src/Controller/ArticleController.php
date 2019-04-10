f9a157445e670641320e7572db0f3eb4710b5491
Nabil MEROUANE <nmerouane@THINK-18-1-PC.stepnetfr.local>
2019-04-10 16:30:40 +0200

    knpMarkDownBundle

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,31 +2,47 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ArticleController extends AbstractController {
 
   /**
-  * @Route("/")
+  * @Route("/", name="app_homepage")
   */
   public function homepage() {
-    return new Response("Hello stepnet !");
+    // return new Response("Hello stepnet !");
+    return $this->render('article/homepage.html.twig');
   }
 
   /**
-  * @Route("/news/{slug}")
+  * @Route("/news/{slug}", name="article_show")
   */
-  public function show($slug)
-  {
-    $members = [
-      'Jamie', 'Cersei', 'Tyrion', 'Geoffrey',
+  public function show($slug) {
+    $comments = [
+      'Nul', 'Magnifique', 'Comment on démarre symfony ?', 'Sensiolabs are genius...',
     ];
 
+    // dump($slug, $this);
+
     return $this->render('article/show.html.twig', [
-      'title' => ucwords(str_replace('-',' ',$slug)),
-      'members' => $members,
+      'title' => ucwords(str_replace('-', ' ', $slug)),
+      'slug' => $slug,
+      'comments' => $comments,
     ]);
+  }
+
+  /**
+  * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+  */
+  public function toggleArticleHeart($slug, LoggerInterface $logger) {
+
+    $logger->info('Article is being hearted');
+    return new JsonResponse(['hearts' => rand(5, 100)]);
+
   }
 }

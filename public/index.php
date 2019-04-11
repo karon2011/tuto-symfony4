5dcc3198691aaacdad60004ee8b3badf310a8633
Nabil MEROUANE <nmerouane@THINK-18-1-PC.stepnetfr.local>
2019-04-11 18:13:58 +0200

    environment dev prod kernel

--- public/index.php
+++ public/index.php
@@ -3,12 +3,30 @@
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+// require dirname(__DIR__).'/config/bootstrap.php';
+//
+// if ($_SERVER['APP_DEBUG']) {
+//     umask(0000);
+//
+//     Debug::enable();
+// }
 
-if ($_SERVER['APP_DEBUG']) {
+require __DIR__.'/../vendor/autoload.php';
+
+// The check is to ensure we don't use .env in production
+if (!isset($_SERVER['APP_ENV'])) {
+    if (!class_exists(Dotenv::class)) {
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+    }
+    (new Dotenv())->load(__DIR__.'/../.env');
+}
+
+$env = $_SERVER['APP_ENV'] ?? 'dev';
+$debug = $_SERVER['APP_DEBUG'] ?? ('prod' !== $env);
+if ($debug) {
     umask(0000);
-
     Debug::enable();
 }
 
@@ -20,7 +38,8 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts([$trustedHosts]);
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+// $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
